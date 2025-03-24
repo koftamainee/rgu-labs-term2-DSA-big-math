264d575a884feb7151daa7fb0be3ef5ab8809c53
koftamainee <koftamainee@gmail.com>
2025-03-24 16:16:44 +0300

    add from_str method

--- src/bigcomplex/io.cpp
+++ src/bigcomplex/io.cpp
@@ -1,8 +1,25 @@
+#include <sstream>
 #include <stdexcept>
 
 #include "bigcomplex.h"
 
-void bigcomplex::from_str(cstd::string const &complex_str) {}  // TODO
+void bigcomplex::from_str(cstd::string const &complex_str) {
+  std::istringstream input(complex_str.c_str());
+  bigfloat real;
+  bigfloat imaginary;
+  char c;
+  input >> real >> c;
+
+  if (c != '+' && c != '-') {
+    throw std::invalid_argument("Invalid complex number format");
+  }
+  input >> imaginary >> c;
+  if (c != 'i') {
+    throw std::invalid_argument("Invalid complex number format");
+  }
+  real_ = std::move(real);
+  imaginary_ = std::move(imaginary);
+}
 
 std::ostream &operator<<(std::ostream &out, bigcomplex const &complex) {
   if (complex.imaginary_ < 0) {
