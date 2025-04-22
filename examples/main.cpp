4a00d68f3c79197918a263864d23e915da5dd3db
koftamainee <koftamainee@gmail.com>
2025-04-22 19:32:17 +0300

    add comments

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,42 +1,34 @@
 #include <cassert>
 #include <climits>
-#include <exception>
 #include <iostream>
-#include <ostream>
 
 #include "bigint.h"
 
 int main() {
-  // bigint a = INT_MIN;
-  // int coeff = 2;
+  // /* Negative remainder ???? should not happens
+  //  * proper output does not works bc it ?? */
+  // bigint a = INT_MAX;
+  // int coeff = 999999999;
   // a *= coeff;
   // std::cout << a << std::endl;
-  // auto dr = bigint::division(a, coeff);
+  // auto dr = bigint::division(a, 52);
   // std::cout << dr.quotient() << " " << dr.remainder() << std::endl;
-  // std::cout << a << std::endl;
-  //
-  // bigint a = INT_MIN;
+
+  // /* Quotient became larger instead of smaller, mb bc prev problem, or bug in
+  //  * right/left shift idk */
+  // bigint a = INT_MAX;
   // while (a > 0) {
-  //   auto dr = bigint::division(a, 10);
+  //   auto dr = bigint::division(a * 15, 10);
   //   std::cout << a << " / 10 == (" << dr.remainder() << ", " << dr.quotient()
   //             << ")" << std::endl;
   //   a = dr.quotient();
   //   getchar();
   // }
-  //
-  // int arr[] = {1, 2, 3, 4, 5};
-  // bigint a(arr, 5);
-  // std::cout << a * a << std::endl;
 
-  int arr[] = {INT_MIN, 0};
-  bigint a(arr, 2);
-  for (int i = 0; i < 128; ++i) {
-    std::cout << (a >>= 1) << std::endl;
-  }
-
-  // int a = 1 << 31;
-  // std::cout << a << std::endl;
-  // for (int i = 0; i < 64; ++i) {
+  // /* Should it be [-1, 0] all the time with shift higher then 32 ??? */
+  // int arr[] = {INT_MIN, 0};
+  // bigint a(arr, 2);
+  // for (int i = 0; i < 128; ++i) {
   //   std::cout << (a >>= 1) << std::endl;
   // }
 }
--- src/bigint/io.cpp
+++ src/bigint/io.cpp
@@ -4,24 +4,24 @@
 #include "cstring.h"
 
 std::ostream &operator<<(std::ostream &out, bigint const &num) noexcept {
-  // auto const digits_count = num.size();
-  // if (digits_count > 1) {
-  //   out << "[";
-  // }
-  // for (int i = 0; i < num.size(); ++i) {
-  //   out << const_cast<bigint &>(num)[i];
-  //   if (i != num.size() - 1) {
-  //     out << ", ";
-  //   }
-  // }
-  // if (digits_count > 1) {
-  //   out << "]";
-  // }
-  //
-  // return out;
+  auto const digits_count = num.size();
+  if (digits_count > 1) {
+    out << "[";
+  }
+  for (int i = 0; i < num.size(); ++i) {
+    out << const_cast<bigint &>(num)[i];
+    if (i != num.size() - 1) {
+      out << ", ";
+    }
+  }
+  if (digits_count > 1) {
+    out << "]";
+  }
 
-  out << num.to_string();
   return out;
+
+  // out << num.to_string();
+  // return out;
 }
 std::istream &operator>>(std::istream &in, bigint &num) noexcept {
   cstd::string input;
